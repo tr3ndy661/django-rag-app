ba109a83952594aaa4fbc3161c3554ad60d28a1e
Abdelmoneim <trendypotato661@gmail.com>
2026-08-23 02:40:55 +0300

    rag done connected to views and llm tested aswell command in readme

--- assistant/llm.py
+++ assistant/llm.py
@@ -1,16 +1,15 @@
 from openai import OpenAI
-
 client = OpenAI(
     base_url="http://localhost:1234/v1",
     api_key='lm-studio'
 )
 
 
-def ask_llm(question):
+def ask_llm(question, context):
     completion = client.chat.completions.create(
     model="qwen2.5-coder-1.5b-instruct",
     messages=[
-        {"role": "user", "content": question}
+            {"role": "user", "content": f"Using the following information: {context}\n\nAnswer this question: {question}"}
     ],
     temperature=0.7,
     )
--- assistant/views.py
+++ assistant/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .llm import ask_llm
+from .retrieval import find_best_doc
 # Create your views here.
 
 @api_view(['POST'])
 def ask(request):
     question = request.data.get ('question')
-    return Response (ask_llm(question))
+    context = find_best_doc(question)
+    return Response (ask_llm(question, context))
 
 
